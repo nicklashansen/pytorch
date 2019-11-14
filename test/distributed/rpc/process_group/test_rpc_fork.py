221e8335359fee7cd9cda579f871d5fa0108e354
Shihao Xu <xsh6528@gmail.com>
2019-11-13 18:11:27 -0800

    Refactoring torch.distributed.rpc tests to have Mixins and Fixtures
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29747
    
    We plan to introduce different `RpcAgentOption`s, this requires us to allow different tests for differnet `RpcAgent`s to correctly configure the test by bringing in different test mixins. This refactoring PR is empowering this.
    
    Also, with more fixtures and mixins, moved tests into sub-folders.
    
    Differential Revision: [D5689636](https://our.internmc.facebook.com/intern/diff/D5689636/)
    ghstack-source-id: 93885275

---
+++ test/distributed/rpc/process_group/test_rpc_fork.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import torch
+from common_distributed import MultiProcessTestCase
+from common_utils import run_tests
+from distributed.rpc.process_group.rpc_test import ProcessGroupRpcTest
+
+import sys
+print("========== sys.path:", sys.path)
+
+# rpc_fork tests use double as the default dtype
+torch.set_default_dtype(torch.double)
+
+
+class ProcessGroupRpcTestWithFork(MultiProcessTestCase, ProcessGroupRpcTest):
+    def setUp(self):
+        super(ProcessGroupRpcTestWithFork, self).setUp()
+        self._fork_processes()
+
+
+if __name__ == "__main__":
+    run_tests()
